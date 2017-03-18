08b230dbfa26c0f1005346ccd085ad3c041e9ece
github username <github email address>
2017-03-18 16:08:51 -0400

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -13,10 +13,18 @@ def first_challenge
     }
   }
 
-  #your code here
-
-
+  contacts["Freddy Mercury"].each do |attribute, value|
+    #first iterate thru all the attributes & their values
+    if attribute == :favorite_icecream_flavors
+      #if the attribute is :favorite_icecream_flavors
+      value.delete_if do |flavor|
+        #go thru all of the flavors (value) within that attribute
+        #if the flavor is "strawberry"
+        #then delete that flavor
+        flavor == "strawberry"
+      end
+    end
+  end
   #remember to return your newly altered contacts hash!
   contacts
 end
-
